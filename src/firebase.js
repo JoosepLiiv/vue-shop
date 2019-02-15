41c56f5ca7f42c83e9a9205266ca771cc0911ef3
Joosep <joosepliiv97@gmail.com>
2019-02-15 20:02:06 +0000

    Firebase integration

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+  import firebase from 'firebase';
+  
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBwUCyJmL8Sir78rJPI68D-zQBcQ8TjkQk",
+    authDomain: "vue-shop-c37b0.firebaseapp.com",
+    databaseURL: "https://vue-shop-c37b0.firebaseio.com",
+    projectId: "vue-shop-c37b0",
+    storageBucket: "vue-shop-c37b0.appspot.com",
+    messagingSenderId: "105714700629"
+  };
+  firebase.initializeApp(config);
